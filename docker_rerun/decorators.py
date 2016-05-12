5c3012a4f368d6671887521100e8d8c6ae0b4a2b
Bradley Cicenas <bradley.cicenas@gmail.com>
2016-05-12 14:41:32 -0400

    add decorator comments

--- docker_rerun/decorators.py
+++ docker_rerun/decorators.py
@@ -1,5 +1,9 @@
 
 def value_opt(f):
+    """
+    value_opt decorator wraps a function returning an option name
+    and a single or multiple values
+    """
     def wrap(obj, *args, **kwargs):
         opt, val = f(obj)
         if not val:
@@ -9,10 +13,14 @@ def value_opt(f):
         elif isinstance(val, list):
             return ' '.join([ '%s%s' % (opt, v) for v in val ])
         else:
-            raise TypeError('unknown option value type: %s' % val)
+            raise TypeError('unsupported option value type: %s' % val)
     return wrap
 
 def bool_opt(f):
+    """
+    bool_opt decorator wraps a function returning an option name and a
+    value that can be evaluated to empty/non-empty
+    """
     def wrap(obj, *args, **kwargs):
         opt, val = f(obj)
         if not val:
